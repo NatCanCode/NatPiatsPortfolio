9c217058938a7a303b8ed018b50a2b670c06492a
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2021-08-31 21:18:46 +0200

    Update french.js

--- script/french.js
+++ script/french.js
@@ -11,5 +11,17 @@ function salutations() {
 }
 salutations();
 
+
+let carousel = document.getElementById("carousel")
+slides = carousel.getElementsByClassName("slider")
+counter = 0
+
+setInterval(function () {
+  slides[counter].style.opacity = 0; // Hide the previous image
+  counter = (counter + 1) % slides.length; // Increment counter
+  slides[counter].style.opacity = 1; // Show the next image
+}, 3000); // setInterval
+
+
 // console.log(++[[]][+[]]+[+[]]);
 // outputs 10
