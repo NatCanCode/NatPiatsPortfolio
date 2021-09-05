9a45dd3b7496ee6e542179ac3749978ecdb090aa
NatCanCode <77299658+NatCanCode@users.noreply.github.com>
2021-09-05 18:33:11 +0200

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -39,6 +39,26 @@ function greetings() {
 greetings()
 
 
+// letter animation
+// wrap every letter in a span
+const textWrapper = document.querySelector('.work .letters')
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+anime.timeline({loop: true})
+    .add({
+        targets: '.work .letter',
+        rotateY: [-90, 0],
+        duration: 1300,
+        delay: (el, i) => 45 * i
+    })
+    .add({
+        targets: '.work',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    })
+
+
 // carousel
 let carousel = document.getElementById("carousel")
 slides = carousel.getElementsByClassName("slider")
--- script/french.js
+++ script/french.js
@@ -38,6 +38,27 @@ darkMode.addEventListener('change' , () => {
 })
 
 
+// letter animation
+// wrap every letter in a span
+const textWrapper = document.querySelector('.work .letters')
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>")
+anime.timeline({loop: true})
+    .add({
+        targets: '.work .letter',
+        rotateY: [-90, 0],
+        duration: 1300,
+        delay: (el, i) => 45 * i
+    })
+    .add({
+        targets: '.work',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    })
+
+
+// carousel
 let carousel = document.getElementById("carousel")
 slides = carousel.getElementsByClassName("slider")
 counter = 0
@@ -295,6 +316,6 @@ anime.timeline({loop: true})
         delay: 1000
     })
 
-    
+
 // console.log(++[[]][+[]]+[+[]]);
 // outputs 10
